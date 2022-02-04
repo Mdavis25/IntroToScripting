06beec249685f51c6694ae2a7de00345358abc6a
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-02-04 00:22:47 -0600

    Second change

--- Assignment_1/Question_7.py
+++ Assignment_1/Question_7.py
@@ -1,6 +1,10 @@
+#user enters weight
 weight = input('Enter your weight ')
+#user enters height
 height = input('Enter your height ')
+#finds bmi
 bmi = int(weight)*703/int(height)**2
+#finds if user is underweight, overweight or optimal
 if bmi < 18.5:
     print('Underweight')
 if bmi >= 18.5 <= 25:

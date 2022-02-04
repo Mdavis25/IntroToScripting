4ccb7256c0132d019e3c43aa6c6bb54900c67a77
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-02-03 20:55:35 -0600

    Inital Commit

---
+++ Assignment_1/Question_3.py
@@ -0,0 +1,6 @@
+#sets the speed
+speed = 70
+#prints information
+print('Distance after 6 hours', speed*6, 'miles')
+print('Distance after 10 hours', speed*10, 'miles')
+print('Distance after 15 hours', speed*15, 'miles')

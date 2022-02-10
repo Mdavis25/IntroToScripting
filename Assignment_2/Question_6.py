007db0ac7c2cff2eca2e9d2c47c4341498087469
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-02-10 17:08:39 -0600

    initial commit

---
+++ Assignment_2/Question_6.py
@@ -0,0 +1,154 @@
+"""
+Question 1
+"""
+#First set of *, uses nested loop to print * followed by spaces using counter
+for temp in range(1, 6):
+    for temp2 in range(0,temp):
+        print("*", end = " ")
+    print()
+
+#Second set of *, uses two nested loops to print spaces followed by * using a counter
+for temp in range(1, 6):
+    for temp2 in range(0, 5 - temp):
+        print(end = "  ")
+    for temp3 in range(0, temp):
+        print("*", end = " ")
+    print()
+
+"""
+Question 2
+"""
+#User enters numbers to factorial, finds the difference
+numN = int(input("Enter First Number: "))
+numR = int(input("Enter Second Number: "))
+diff = numN - numR
+
+#sets factorial for loop counters
+factN = 1
+factR = 1
+factDiff = 1
+
+#finds factorial of N
+for temp in range(1, numN+1):
+    factN = factN * temp
+
+#finds factorial of R
+for temp in range(1, numR+1):
+    factR = factR * temp
+
+#Calculates the difference of N and R then factorials it
+for temp in range(1, diff+1):
+    factDiff = factDiff * temp
+
+#finds total of (n-r)!
+total = factR*factDiff
+
+#prints total
+print("n!/(r!(n-r)!) is equal to: ", factN/(factR*total))
+print("n!/(n-r)! is equal to: ", factN/factDiff)
+
+"""
+Question 3
+"""
+#setting lists to be sorted
+List = [20, 68, 41, 88, 16, 40, 65, 97, 85]
+newList = []
+
+#sets length equal to the number of items in List
+length = len(List)
+
+#while the length of newList is not equal to the length, this loop will run
+while len(newList) != length:
+    #setting a zero to check the first spot in the list
+    smallestIndex = 0
+    #loop to go through list
+    for tempIndex in range(0, len(List)):
+        #if the second number is smaller then the first, it makes the first second number the smallest
+        if (List[smallestIndex] > List[tempIndex]):
+            smallestIndex = tempIndex
+    #removes smallest number from the old list
+    tempVal = List.pop(smallestIndex)
+    #adds smallest number to the new list
+    newList.append(tempVal);
+
+#prints old and new lists
+print("Old list: ", List)
+print("Sorted list: ", newList);
+
+"""
+Question 6
+"""
+def firstMethod():
+    # First set of *, uses nested loop to print * followed by spaces using counter
+    for temp in range(1, 6):
+        for temp2 in range(0, temp):
+            print("*", end=" ")
+        print()
+
+    # Second set of *, uses two nested loops to print spaces followed by * using a counter
+    for temp in range(1, 6):
+        for temp2 in range(0, 5 - temp):
+            print(end="  ")
+        for temp3 in range(0, temp):
+            print("*", end=" ")
+        print()
+
+
+def secondMethod(n, r):
+    # User enters numbers to factorial, finds the difference
+    numN = int(input("Enter First Number: "))
+    numR = int(input("Enter Second Number: "))
+    diff = numN - numR
+
+    # sets factorial for loop counters
+    factN = 1
+    factR = 1
+    factDiff = 1
+
+    # finds factorial of N
+    for temp in range(1, numN + 1):
+        factN = factN * temp
+
+    # finds factorial of R
+    for temp in range(1, numR + 1):
+        factR = factR * temp
+
+    # Calculates the difference of N and R then factorials it
+    for temp in range(1, diff + 1):
+        factDiff = factDiff * temp
+
+    # finds total of (n-r)!
+    total = factR * factDiff
+
+    # prints total
+    print("n!/(r!(n-r)!) is equal to: ", factN / (factR * total))
+    print("n!/(n-r)! is equal to: ", factN / factDiff)
+
+
+def thirdMethod(List):
+    # setting lists to be sorted
+    List = [20, 68, 41, 88, 16, 40, 65, 97, 85]
+    newList = []
+
+    # sets length equal to the number of items in List
+    length = len(List)
+
+    # while the length of newList is not equal to the length, this loop will run
+    while len(newList) != length:
+        # setting a zero to check the first spot in the list
+        smallestIndex = 0
+        # loop to go through list
+        for tempIndex in range(0, len(List)):
+            # if the second number is smaller then the first, it makes the first second number the smallest
+            if (List[smallestIndex] > List[tempIndex]):
+                smallestIndex = tempIndex
+        # removes smallest number from the old list
+        tempVal = List.pop(smallestIndex)
+        # adds smallest number to the new list
+        newList.append(tempVal);
+
+    # prints old and new lists
+    print("Old list: ", List)
+    print("Sorted list: ", newList);
+
+

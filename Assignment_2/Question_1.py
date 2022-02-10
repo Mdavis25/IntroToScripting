d5fa25630eb753c24e013b83103d5cbccfc00c31
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-02-10 14:42:18 -0600

    second commit

--- Assignment_2/Question_1.py
+++ Assignment_2/Question_1.py
@@ -1,10 +1,10 @@
-#
+#First set of *, uses nested loop to print * followed by spaces using counter
 for temp in range(1, 6):
     for temp2 in range(0,temp):
         print("*", end = " ")
     print()
 
-#
+#Second set of *, uses two nested loops to print spaces followed by * using a counter
 for temp in range(1, 6):
     for temp2 in range(0, 5 - temp):
         print(end = "  ")

8507ab2e931df9a2ad06ebcd023570f8ee26d440
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-02-22 19:06:28 -0600

    Second Commit

--- Assignment_3/Question_3.py
+++ Assignment_3/Question_3.py
@@ -1,25 +1,25 @@
-class Employee():
+# Employee Class
+class Employee:
     def __init__(self, fName, lName, idNum, dept, title):
+        # Declaring employee Variables
         self.firstName = fName
         self.lastName = lName
         self.idNumber = idNum
         self.department = dept
         self.jobTitle = title
 
-        self.fullName = self.firstName + " " + self.lastName
+        # setting employees full name
+        self.full = self.firstName + " " + self.lastName
+        # setting employees email
         self.email = self.firstName.lower() + "." + self.lastName.lower() + "@company.com"
 
 
+# main
 if __name__ == '__main__':
-    def __str__(self):
-        return 'Name: {self.firstName} {self.lastName}\nIdNumber: {self.idNumber}\nDepartment: {self.department}\nJob ' \
-               'Title: {self.jobTitle}\n'.format(self=self)
-
-
     empOne = Employee("Susan", "Meyers", 47899, "Accounting", "Vice President")
     empTwo = Employee("Mark", "Jones", 39119, "IT", "Programmer")
     empThree = Employee("Joy", "Rogers", 81774, "Manufacturing", "Engineer")
 
-    print("Employee One Full Name: ", empOne.fullName)
+    print("Employee One Full Name: ", empOne.full)
     print("Employee One Email: ", empOne.email)
     print()

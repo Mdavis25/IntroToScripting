d1fe6a469085c8a8069a83a2fa7040baefe8a7a6
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-03-08 19:48:23 -0600

    Second Commit

--- Assignment_5/Question_2.py
+++ Assignment_5/Question_2.py
@@ -1,28 +1,32 @@
-def getNumVows(someStr):
+# function to find Vowels
+def VowelCounter(uInput):
     count = 0
-
-    for ch in someStr:
+    # for loop to count vowels in the string
+    for ch in uInput:
         if ch.upper() == 'A' or ch.upper() == 'E' or ch.upper() == 'I' or ch.upper() == 'O' or ch.upper() == 'U':
             count += 1
-
+    # returns count
     return count
 
 
-def getNumCons(someStr):
+# function to find consonants
+def ConCounter(uInput):
     count = 0
-
-    for ch in someStr:
+    # for loop to count consonants in the string
+    for ch in uInput:
         if ch.isalpha() and ch.upper() != 'A' and ch.upper() != 'E' and ch.upper() != 'I' and ch.upper() != 'O' and ch.upper() != 'U':
             count += 1
-
+    # returns count
     return count
 
 
-def partTwo():
-    userIn = input("Please enter a string to count vowels and consonants: ")
-    print("Number of vowels in string: ", getNumVows(userIn))
-    print("Number of consonants in string: ", getNumCons(userIn))
+# function to call in main that will call other functions
+def Counter():
+    uInput = input("Enter a string to count consonants and vowels: ")
+    print("Number of vowels in the string: ", VowelCounter(uInput))
+    print("Number of consonants in the string: ", ConCounter(uInput))
 
 
+# main
 if __name__ == '__main__':
-    partTwo()
+    Counter()

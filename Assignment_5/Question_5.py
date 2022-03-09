eebb81e4fc81a54a3fa187472c0d441f229128c2
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-03-08 21:12:55 -0600

    Second Commit

--- Assignment_5/Question_5.py
+++ Assignment_5/Question_5.py
@@ -1,63 +1,63 @@
-def partFive():
+# function to call in main
+def question5():
+    # main string
     mainStr = "this is the string for the class"
 
-    copyOne = ""
-    copyTwo = ""
-    copyThree = ""
-    copyFour = ""
+    stringOne = ""
+    stringTwo = ""
+    stringThree = ""
+    stringFour = ""
 
-    beforeNum = 0
+    count = 0
 
+    # uses a for loop to go through string
     for num in range(0, len(mainStr)):
-
+        # changes the first letter to a capital
         if num == 0:
-            copyOne = copyOne + mainStr[num].upper()
-
-        elif mainStr[beforeNum] == ' ':
-            copyOne = copyOne + mainStr[num].upper()
-
+            stringOne = stringOne + mainStr[num].upper()
+        # changes the first letter of each word after the space to a capital
+        elif mainStr[count] == ' ':
+            stringOne = stringOne + mainStr[num].upper()
+        # copies the rest of the sentence
         else:
-            copyOne = copyOne + mainStr[num]
+            stringOne = stringOne + mainStr[num]
+        count = num
+    # prints new string
+    print(stringOne)
 
-        beforeNum = num
-
-    print(copyOne)
-
-    for ch in copyOne:
-
+    # for loop to remove spaces in sentence
+    for ch in stringOne:
         if ch != ' ':
-            copyTwo = copyTwo + ch
+            stringTwo = stringTwo + ch
+    # prints new string
+    print(stringTwo)
 
-    print(copyTwo)
-
-    for ch in copyOne:
-
+    # for loop to change all the s in string to $
+    for ch in stringOne:
         if ch == 's':
-            copyThree = copyThree + '$'
-
+            stringThree = stringThree + '$'
         else:
-            copyThree = copyThree + ch
+            stringThree = stringThree + ch
+    # prints new string
+    print(stringThree)
+    count = 0
 
-    print(copyThree)
-
-    beforeNum = 0
-
+    # for loop to change some letters into uppercase
     for num in range(0, len(mainStr)):
-
+        # changes the first letter to uppercase
         if num == 0:
-            copyFour = copyFour + mainStr[num].upper()
-
-        elif mainStr[beforeNum] == ' ' and mainStr[num] == 's' or mainStr[num] == 'c':
-            copyFour = copyFour + mainStr[num].upper()
-
+            stringFour = stringFour + mainStr[num].upper()
+        # changes the S and C to uppercase
+        elif mainStr[count] == ' ' and mainStr[num] == 's' or mainStr[num] == 'c':
+            stringFour = stringFour + mainStr[num].upper()
+        # copies the rest of the string
         else:
-            copyFour = copyFour + mainStr[num]
-
-        beforeNum = num
-
-    # print the new string
-    print(copyFour)
+            stringFour = stringFour + mainStr[num]
+        count = num
+    # prints the string
+    print(stringFour)
 
 
+# main
 if __name__ == '__main__':
-    partFive()
+    question5()

7b84f804882fc7612d8e93c0bf1cf82a30692517
Matthew Davis <mdavis25@islander.tamucc.edu>
2022-03-08 19:43:22 -0600

    Second Commit

--- Assignment_5/Question_1.py
+++ Assignment_5/Question_1.py
@@ -1,54 +1,61 @@
-def partOne():
-    morseCode = {' ': ' ',
-                 ',': '--..--',
-                 '.': '.-.-.-',
-                 '?': '..--..',
-                 '0': '-----',
-                 '1': '.----',
-                 '2': '..---',
-                 '3': '...--',
-                 '4': '....-',
-                 '5': '.....',
-                 '6': '-....',
-                 '7': '--...',
-                 '8': '---..',
-                 '9': '----.',
-                 'A': '.-',
-                 'B': '-...',
-                 'C': '-.-.',
-                 'D': '-..',
-                 'E': '.',
-                 'F': '..-.',
-                 'G': '--.',
-                 'H': '....',
-                 'I': '..',
-                 'J': '.---',
-                 'K': '-.-',
-                 'L': '.-..',
-                 'M': '--',
-                 'N': '-.',
-                 'O': '---',
-                 'P': '.--.',
-                 'Q': '--.-',
-                 'R': '.-.',
-                 'S': '...',
-                 'T': '-',
-                 'U': '..-',
-                 'V': '...-',
-                 'W': '.--',
-                 'X': '-..-',
-                 'Y': '-.--',
-                 'Z': '--..'}
+# morse code class to call in main
+def MorseCode():
+    # morse code dictionary
+    charDict = {' ': ' ',
+                ',': '--..--',
+                '.': '.-.-.-',
+                '?': '..--..',
+                '0': '-----',
+                '1': '.----',
+                '2': '..---',
+                '3': '...--',
+                '4': '....-',
+                '5': '.....',
+                '6': '-....',
+                '7': '--...',
+                '8': '---..',
+                '9': '----.',
+                'A': '.-',
+                'B': '-...',
+                'C': '-.-.',
+                'D': '-..',
+                'E': '.',
+                'F': '..-.',
+                'G': '--.',
+                'H': '....',
+                'I': '..',
+                'J': '.---',
+                'K': '-.-',
+                'L': '.-..',
+                'M': '--',
+                'N': '-.',
+                'O': '---',
+                'P': '.--.',
+                'Q': '--.-',
+                'R': '.-.',
+                'S': '...',
+                'T': '-',
+                'U': '..-',
+                'V': '...-',
+                'W': '.--',
+                'X': '-..-',
+                'Y': '-.--',
+                'Z': '--..'}
 
-    userIn = input("Please enter a string to translate to morse code: ")
+    # User inputs string to be translated
+    uInput = input("Please enter a string to translate to morse code: ")
 
+    # holds the morse code while each letter is translated
     translate = ""
 
-    for ch in userIn:
-        translate = translate + morseCode[ch.upper()]
+    # for loop to translate from dictionary
+    for ch in uInput:
+        translate = translate + charDict[ch.upper()]
 
-    print("Morse code of ", userIn, " is ", translate)
+    # prints translated string
+    print("Morse code of ", uInput, " is ", translate)
 
 
+# main to call morse code function
 if __name__ == '__main__':
-    partOne()
+    MorseCode()
